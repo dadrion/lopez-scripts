edf517c7da0dcc96210bd6facc7fcde5e2876979
dadrion <91682031+dadrion@users.noreply.github.com>
2022-12-09 11:27:41 -0500

    Update h_bonding_analysis.py
    I added comments to make this a little more descriptive. Thanks

--- h_bonding_analysis.py
+++ h_bonding_analysis.py
@@ -14,10 +14,12 @@ import matplotlib.pyplot as plt
 from typing import List
 
 assert len(sys.argv) == 3
-ROOT_DIR = sys.argv[1]
-NAME = sys.argv[2]
+ROOT_DIR = sys.argv[1] # Directory to run analysis in
+NAME = sys.argv[2] # Parent name of files to measure
 
-
+"""
+Specify where the root is, which files are needed, and define the numbers within the trajectories
+"""
 def run(root_dir: str, name: str) -> None:
     ntraj = len(glob.glob1(ROOT_DIR,"*.dat"))
     last_step_hash: List[int] = []
@@ -28,7 +30,10 @@ def run(root_dir: str, name: str) -> None:
             cur_largest_step = len(f.readlines())
             last_step_hash.append(cur_largest_step)
             largest_step = max(largest_step, cur_largest_step)
-
+"""
+Plot the H-bonding frequency by counting if the number of H-bonds in a given snapshot is >0
+This will also append the instances of H bonding into a list
+"""
     h_bonding_freq = []
     for step_num in range(largest_step):
         h_bonding_count = 0
@@ -48,7 +53,7 @@ def run(root_dir: str, name: str) -> None:
         h_bonding_freq.append(round(h_bonding_count / valid_traj_count, 2))
 
     plot(h_bonding_freq, ntraj)
-
+#Plot h-bond frequency data (this can be changed as-needed)
 def plot(data: List[int], ntraj: int) -> None:
     plt.plot(list(range(ntraj)), data)
     plt.title('H Bonding Analysis')
